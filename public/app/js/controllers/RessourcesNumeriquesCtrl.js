c40e0b188aba2ad8668b41bb4bd6a053c0889c89
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-09-08 17:01:56 +0200

    refactoring CCNctrl & Ressourcesnumeriquesctrl

--- public/app/js/controllers/RessourcesNumeriquesCtrl.js
+++ public/app/js/controllers/RessourcesNumeriquesCtrl.js
@@ -12,21 +12,32 @@ angular.module( 'portailApp' )
                                    $state.go( 'portail.logged', {}, { reload: true, inherit: true, notify: true } );
                                }
 
-                               var log_and_open_link = function( context, url ) {
-                                   log.add( context, url, null );
-                                   $window.open( url, 'laclasseexterne' );
-                               };
-
                                currentUser.ressources().then( function ( response ) {
+                                   var pad_tiles_tree = function( tiles_tree ) {
+                                       if ( tiles_tree.length < CASES.length ) {
+                                           return tiles_tree.concat( _.chain(CASES.slice( tiles_tree.length, CASES.length )).map( function( c ) {
+                                               c.couleur += '-moins';
+                                               return c;
+                                           } ).value() );
+                                       } else {
+                                           return tiles_tree;
+                                       }
+                                   };
+
                                    $scope.root = { class: 'ressources-numeriques',
                                                    laius: $sce.trustAsHtml( '<p class="laius">Retrouvez ici les ressources numériques que votre établissement a sélectionné pour vous. Ces ressources peuvent être des manuels scolaires en ligne, des dictionnaires, des sites proposant des ressources pour s\'entraîner, etc...</p><p class="laius"> Si rien n\'est affiché dans cette page, c\'est que votre établissement n\'a ps encore activé les ressources numériques. Vous pouvez contacter un de vos administrateur de l\'ENT pour lui demander l\'activation de celles-ci.</p>' ),
                                                    tiles: _(response).map( function( rn, i ) {
-                                                       return { nom: rn.lib,
-                                                                description: rn.nom_court,
-                                                                action: function() { log_and_open_link( 'GAR', rn.url_access_get ); },
-                                                                icon: '/app/node_modules/laclasse-common-client/images/' + rn.icone,
-                                                                couleur: CASES[ i % 16 ].couleur };
+                                                       rn.icon = '/app/node_modules/laclasse-common-client/images/' + rn.icon;
+                                                       rn.couleur = CASES[ i % 16 ].couleur;
+                                                       rn.action = function() {
+                                                           log.add( 'GAR', rn.url, null );
+                                                           $window.open( rn.url, 'laclasseexterne' );
+                                                       };
+
+                                                       return rn;
                                                    } ) };
+
+                                   $scope.root.tiles = pad_tiles_tree( $scope.root.tiles );
                                } );
                            } );
                    }
