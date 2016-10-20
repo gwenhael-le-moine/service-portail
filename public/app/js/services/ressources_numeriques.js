3dff0a705bf6bdf22ebe15c704e138582a55cace
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-10-20 15:30:49 +0200

    plug RessourceNumerique into the annuaire

--- public/app/js/services/ressources_numeriques.js
+++ public/app/js/services/ressources_numeriques.js
@@ -4,5 +4,13 @@ angular.module( 'portailApp' )
     .factory( 'RessourceNumerique',
               [ '$resource', 'URL_ENT',
                 function( $resource, URL_ENT ) {
-                    return $resource( URL_ENT + 'api/ressources/:id' );
+                    return $resource( URL_ENT + 'api/etablissements/:id/ressources/:ressource_id',
+                                      { id : '@id',
+                                        ressource_id : '@ressource_id',
+                                        ressource_num_id : '@ressource_num_id',
+                                        date_deb_abon : '@date_deb_abon',
+                                        date_fin_abon : '@date_fin_abon' },
+                                      { query_default: { methode: 'GET',
+                                                         url: URL_ENT + 'api/ressources/',
+                                                         isArray: true } });
                 } ] );
