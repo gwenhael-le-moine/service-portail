dbc5cfc1afa5ad3920326ca3c322da56b428fd26
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-10-27 15:59:15 +0200

    avatar component sole owner of avatar manipulation

--- public/app/js/components/avatar.js
+++ public/app/js/components/avatar.js
@@ -5,75 +5,108 @@ angular.module( 'portailApp' )
                 function() {
                     return { restrict: 'A',
                              link: function( scope, element, attrs ) {
-                                 element.bind('change', scope.$eval( attrs.customOnChange ) );
+                                 element.bind('change', scope.$eval( attrs.fileChanged ) );
                              } };
                 } )
     .component( 'avatar',
                 { bindings: { user: '=' },
-                  template: '<img draggable="false" class="svg" ' +
+                  template: '<div class="avatar">' +
+                  '              <img draggable="false" class="svg" ' +
                   '               ng:src="{{$ctrl.user.new_avatar.image ? $ctrl.user.new_avatar.image : $ctrl.user.avatar}}" />' +
-                  '          <input type="file" file-changed="$ctrl.onChange"/>',
-                  controller: [
-                      function(  ) {
-                          var ctrl = this;
+                  '          <input type="file" file-changed="$ctrl.onChange"/>' +
+                  '          <footer>' +
+                  '              <button ng:disabled="!$ctrl.user.new_avatar.blob"' +
+                  '                      ng:click="$ctrl.upload_avatar()">Valider l\'avatar</button>' +
+                  '          </footer>' +
+                  '</div>',
+                  controller: [ 'currentUser',
+                                function( currentUser ) {
+                                    var ctrl = this;
 
-                          var processFile = function( file ) {
-                              var max_height = 256;
-                              var max_width = 256;
+                                    var processFile = function( file ) {
+                                        var max_height = 256;
+                                        var max_width = 256;
 
-                              var blobToDataURL = function( blob, callback ) {
-                                  var a = new FileReader();
-                                  a.onload = function( e ) { callback( e.target.result ); };
-                                  a.readAsDataURL(blob);
-                              };
+                                        var blobToDataURL = function( blob, callback ) {
+                                            var a = new FileReader();
+                                            a.onload = function( e ) { callback( e.target.result ); };
+                                            a.readAsDataURL(blob);
+                                        };
 
-                              blobToDataURL( file,
-                                             function( dataURL ) {
-                                                 ctrl.user.new_avatar.image = dataURL;
+                                        blobToDataURL( file,
+                                                       function( dataURL ) {
+                                                           ctrl.user.new_avatar.image = dataURL;
 
-                                                 var img = new Image();
+                                                           var img = new Image();
 
-                                                 img.onload = function() {
-                                                     ctrl.user.new_avatar.height = img.height;
-                                                     ctrl.user.new_avatar.width = img.width;
+                                                           img.onload = function() {
+                                                               ctrl.user.new_avatar.height = img.height;
+                                                               ctrl.user.new_avatar.width = img.width;
 
-                                                     // Compute new dimensions if necessary
-                                                     var factor = 1;
+                                                               // Compute new dimensions if necessary
+                                                               var factor = 1;
 
-                                                     if ( ctrl.user.new_avatar.width > max_width ) {
-                                                         factor = max_width / img.width;
-                                                         ctrl.user.new_avatar.width = max_width;
-                                                         ctrl.user.new_avatar.height = img.height * factor;
-                                                     }
+                                                               if ( ctrl.user.new_avatar.width > max_width ) {
+                                                                   factor = max_width / img.width;
+                                                                   ctrl.user.new_avatar.width = max_width;
+                                                                   ctrl.user.new_avatar.height = img.height * factor;
+                                                               }
 
-                                                     if ( ctrl.user.new_avatar.height > max_height ) {
-                                                         factor = max_height / img.height;
-                                                         ctrl.user.new_avatar.height = max_height;
-                                                         ctrl.user.new_avatar.width = img.width * factor;
-                                                     }
+                                                               if ( ctrl.user.new_avatar.height > max_height ) {
+                                                                   factor = max_height / img.height;
+                                                                   ctrl.user.new_avatar.height = max_height;
+                                                                   ctrl.user.new_avatar.width = img.width * factor;
+                                                               }
 
-                                                     // create new, resized image blob using canvas
-                                                     var canvas = document.createElement( 'canvas' );
-                                                     canvas.width = ctrl.user.new_avatar.width;
-                                                     canvas.height = ctrl.user.new_avatar.height;
+                                                               // create new, resized image blob using canvas
+                                                               var canvas = document.createElement( 'canvas' );
+                                                               canvas.width = ctrl.user.new_avatar.width;
+                                                               canvas.height = ctrl.user.new_avatar.height;
 
-                                                     var ctx = canvas.getContext( '2d' );
-                                                     ctx.drawImage( img, 0, 0, ctrl.user.new_avatar.width, ctrl.user.new_avatar.height );
+                                                               var ctx = canvas.getContext( '2d' );
+                                                               ctx.drawImage( img, 0, 0, ctrl.user.new_avatar.width, ctrl.user.new_avatar.height );
 
-                                                     canvas.toBlob( function( blob ) {
-                                                         blob.name = file.name;
-                                                         ctrl.user.new_avatar.blob = blob;
-                                                     },
-                                                                    'image/png' );
+                                                               canvas.toBlob( function( blob ) {
+                                                                   blob.name = file.name;
+                                                                   ctrl.user.new_avatar.blob = blob;
+                                                               },
+                                                                              'image/png' );
 
-                                                     ctrl.user.new_avatar.image = canvas.toDataURL();
-                                                 };
-                                                 img.src = ctrl.user.new_avatar.image;
-                                             } );
-                          };
+                                                               ctrl.user.new_avatar.image = canvas.toDataURL();
+                                                           };
+                                                           img.src = ctrl.user.new_avatar.image;
+                                                       } );
+                                    };
 
-                          ctrl.onChange = function( event ) {
-                              processFile( event.target.files[0] );
-                          };
-                      } ]
+                                    ctrl.onChange = function( event ) {
+                                        processFile( event.target.files[0] );
+                                    };
+
+                                    var reset_new_avatar = function() {
+                                        ctrl.user.new_avatar = { image: null,
+                                                                 blob: null,
+                                                                 width: 0,
+                                                                 height: 0 };
+                                    };
+
+                                    ctrl.upload_avatar = function() {
+                                        currentUser.avatar.upload( ctrl.user.new_avatar.blob )
+                                            .then( function( data, status, headers, config ) {
+                                                currentUser.force_refresh();
+                                                reset_new_avatar();
+                                            });
+                                    };
+
+                                    // ctrl.delete_avatar = function() {
+                                    //     currentUser.avatar.delete()
+                                    //         .then( function( response ) {
+                                    //             ctrl.operation_on_avatar = false;
+                                    //             ctrl.uploaded_avatar = null;
+                                    //             currentUser.force_refresh();
+                                    //             reset_new_avatar();
+                                    //         } );
+                                    // };
+
+                                    reset_new_avatar();
+                                } ]
                 } );
--- public/app/js/components/user_profile.js
+++ public/app/js/components/user_profile.js
@@ -35,46 +35,8 @@ angular.module( 'portailApp' )
                                         };
                                     };
 
-                                    var reset_new_avatar = function() {
-                                        ctrl.user.new_avatar = { image: null,
-                                                                 blob: null,
-                                                                 width: 0,
-                                                                 height: 0 };
-                                        delete dirty.new_avatar;
-                                    };
-
-                                    ctrl.reset_avatar = function() {
-                                        ctrl.apply_reset_avatar = true;
-                                    };
-
-                                    ctrl.check_password = function( password ) {
-                                        currentUser.check_password( password ).then( function( response ) {
-                                            return response.valid;
-                                        } );
-                                    };
-
-                                    ctrl.upload_avatar = function() {
-                                        currentUser.avatar.upload( ctrl.user.new_avatar.blob )
-                                            .then( function( data, status, headers, config ) {
-                                                currentUser.force_refresh();
-                                                reset_new_avatar();
-                                            });
-                                    };
-
-                                    ctrl.delete_avatar = function() {
-                                        ctrl.operation_on_avatar = true;
-
-                                        currentUser.avatar.delete()
-                                            .then( function( response ) {
-                                                ctrl.operation_on_avatar = false;
-                                                ctrl.uploaded_avatar = null;
-                                                currentUser.force_refresh();
-                                                reset_new_avatar();
-                                            } );
-                                    };
-
                                     ctrl.fermer = function( sauvegarder ) {
-                                        if ( sauvegarder && ( !_(dirty).isEmpty() || !_(ctrl.user.new_avatar.blob).isNull() ) ) {
+                                        if ( sauvegarder && !_(dirty).isEmpty() ) {
                                             if ( _(ctrl.password.new1).isEmpty() || ( !_(ctrl.password.new1).isEmpty() && ( ctrl.password.new1 == ctrl.password.new2 ) ) ) {
                                                 var mod_user = {};
 
@@ -89,17 +51,7 @@ angular.module( 'portailApp' )
                                                 User.update( mod_user ).$promise
                                                     .then( function( response ) {
                                                         currentUser.force_refresh();
-
-                                                        if ( !_(ctrl.user.new_avatar.blob).isNull() &&
-                                                             ctrl.user.new_avatar.blob.type != "" &&
-                                                             !_(ctrl.user.new_avatar.blob.type.match( "image/.*" )).isNull() ) {
-                                                            ctrl.upload_avatar();
-                                                        } else if ( ctrl.apply_reset_avatar ) {
-                                                            ctrl.delete_avatar();
-                                                        } else {
-                                                            currentUser.force_refresh();
-                                                            Utils.go_home();
-                                                        }
+                                                        Utils.go_home();
                                                     } );
                                             }
                                         } else {
@@ -110,6 +62,5 @@ angular.module( 'portailApp' )
                                     ctrl.user.editable = _(ctrl.user.id_jointure_aaf).isNull();
 
                                     ctrl.user.date_naissance = new Date( ctrl.user.date_naissance );
-                                    reset_new_avatar();
                                 } ]
                 } );
