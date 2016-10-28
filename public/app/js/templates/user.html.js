71a138348db621b0c1ba53aa0181f8013ffddcc1
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-10-28 15:00:13 +0200

    remove leftovers

--- public/app/js/templates/user.html.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-angular.module( 'portailApp' )
-  .run( [ '$templateCache',
-    function( $templateCache ) {
-      $templateCache.put( 'views/user.html',
-                          '<div class="row user-profil">    <userprofile user="current_user"></userprofile></div>' );     } ] );
