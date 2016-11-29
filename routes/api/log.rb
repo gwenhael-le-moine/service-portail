591461b21a61d17439b18607b4249dce1d2a389c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-11-29 13:50:47 +0100

    post log api now useless

--- routes/api/log.rb
+++ routes/api/log.rb
@@ -16,23 +16,6 @@ module Portail
 
             Laclasse::CrossApp::Sender.send_request_signed( :service_annuaire_v2_logs, 'stats', params ).to_json
           end
-
-          #
-          # Tell Annuaire to log this
-          #
-          app.post "#{APP_PATH}/api/log/?" do
-            # param :uid, String, required: true
-            # param :uai, String, required: true
-            # param :action, String, required: true
-            # param :url, String, required: true
-            # param :comment, String
-
-            log_entry = JSON.parse( request.body.read )
-            log_entry['ip'] = request.env[ 'HTTP_X_FORWARDED_FOR' ]
-            log_entry['ip'] ||= 'unknown'
-
-            Laclasse::CrossApp::Sender.post_request_signed( :service_annuaire_v2_logs, '', log_entry, {} )
-          end
         end
       end
     end
