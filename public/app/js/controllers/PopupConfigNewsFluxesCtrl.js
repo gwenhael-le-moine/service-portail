e17be1a28fa805638ac60a515b13cdbb1d1ab152
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-12-02 10:50:13 +0100

    flux talk with annuaire directly

--- public/app/js/controllers/PopupConfigNewsFluxesCtrl.js
+++ public/app/js/controllers/PopupConfigNewsFluxesCtrl.js
@@ -2,51 +2,50 @@
 
 angular.module( 'portailApp' )
     .controller( 'PopupConfigNewsFluxesCtrl',
-                 [ '$scope', '$uibModalInstance', 'Flux', 'CONFIG',
-                   function( $scope, $uibModalInstance, Flux, CONFIG ) {
+                 [ '$scope', '$rootScope', '$uibModalInstance', 'Flux', 'CONFIG',
+                   function( $scope, $rootScope, $uibModalInstance, Flux, CONFIG ) {
+                       var ctrl = $scope;
+
                        Flux.query().$promise.then( function( response ) {
-                           $scope.current_flux = _(response).map( function( flux ) {
+                           ctrl.current_flux = _(response).map( function( flux ) {
                                flux.dirty = false;
 
-                               if ( flux.default ) {
-                                   $scope.save( flux );
-                               }
-
                                return flux;
-                               } );
+                           } );
                        } );
 
-                       $scope.minutes = _.range( 1, 11 );
+                       ctrl.nb_articles = _.range( 1, 11 );
 
-                       $scope.delete = function( flux ) {
+                       ctrl.delete = function( flux ) {
                            flux.$delete();
-                           $scope.current_flux = _($scope.current_flux).difference( [ flux ] );
+                           ctrl.current_flux = _(ctrl.current_flux).difference( [ flux ] );
                        };
 
-                       $scope.save = function( flux ) {
-                           _(flux).has( 'id' ) ? flux.$update() : flux.$save();
+                       ctrl.save = function( flux ) {
+                           flux.etab_code_uai = $rootScope.current_user.profil_actif.etablissement_code_uai;
+                           return _(flux).has( 'id' ) ? flux.$update() : flux.$save();
                        };
 
-                       $scope.dirtify = function( flux ) {
+                       ctrl.dirtify = function( flux ) {
                            flux.dirty = true;
                        };
 
-                       $scope.add_flux = function() {
-                           $scope.current_flux.push( new Flux( {
-                               nb: 1,
-                               title: '',
-                               flux: '',
-                               icon: ''
-                           } ) );
+                       ctrl.add_flux = function() {
+                           ctrl.current_flux.push( new Flux( { nb: 1,
+                                                               title: '',
+                                                               flux: '',
+                                                               icon: '' } ) );
                        };
 
-                       $scope.add_default_flux = function() {
+                       ctrl.add_default_flux = function() {
                            _(CONFIG.news_feed).each( function( flux ) {
-                               $scope.current_flux.push( new Flux( flux ) );
+                               ctrl.save( flux ).then( function( response ) {
+                                   ctrl.current_flux.push( response );
+                               } );
                            } );
                        };
 
-                       $scope.close = function () {
+                       ctrl.close = function () {
                            $uibModalInstance.close();
                        };
                    } ] );
