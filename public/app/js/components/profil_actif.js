ace5c3eeb1ffb7ab4fd71d83d1705a7b2efd6eb4
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-12-09 11:51:20 +0100

    fixed profil actif selectbox

--- public/app/js/components/profil_actif.js
+++ public/app/js/components/profil_actif.js
@@ -4,21 +4,22 @@ angular.module( 'portailApp' )
     .component( 'profilactif',
                 { bindings: { user: '=' },
                   template: '<select ' +
-                  'ng:model="$ctrl.new_profil"' +
-                  'ng:change="$ctrl.apply_change( $ctrl.new_profil )"' +
-                  'ng:options="profil as profil.etablissement_nom + \' : \' + profil.profil_nom group by profil.etablissement_nom for profil in $ctrl.user.profils track by $index" >' +
+                  'ng:model="$ctrl.user.profil_actif"' +
+                  'ng:change="$ctrl.apply_change()"' +
+                  'ng:options="profil as profil.etablissement_nom + \' : \' + profil.profil_nom group by profil.etablissement_nom for profil in $ctrl.user.profils track by profil.index" >' +
                   '</select>',
                   controller: [ '$state', '$stateParams',
                                 function( $state, $stateParams ) {
                                     var ctrl = this;
 
-                                    ctrl.new_profil = angular.copy( ctrl.user.profil_actif );
-                                    console.log(ctrl.new_profil)
+                                    ctrl.user.profils.forEach( function( profil, index ) {
+                                        profil.index = index;
+                                    } );
+                                    ctrl.user.profil_actif = _(ctrl.user.profils).findWhere({ actif: true });
 
-                                    ctrl.apply_change = function( new_profil ) {
-                                        console.log(new_profil)
-                                        ctrl.user.$change_profil_actif( { profil_id: new_profil.profil_id,
-                                                                          uai: new_profil.etablissement_code_uai } )
+                                    ctrl.apply_change = function() {
+                                        ctrl.user.$change_profil_actif( { profil_id: ctrl.user.profil_actif.profil_id,
+                                                                          uai: ctrl.user.profil_actif.etablissement_code_uai } )
                                             .then( function() {
                                                 $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
                                             });
