398997bff0fd01ad4ae553f0d67ec96f3b4e44d8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-01-17 15:10:18 +0100

    wait for component to be initialized

--- public/app/js/components/help_icon.js
+++ public/app/js/components/help_icon.js
@@ -13,11 +13,13 @@ angular.module( 'portailApp' )
                   '        </div>',
                   controller: [ 'CONFIG',
                                 function( CONFIG ) {
-                                    var ctrl = this;
+                                    let ctrl = this;
 
-                                    ctrl.help_links = _(CONFIG.help_links)
-                                        .select( function( link ) {
-                                            return !_(ctrl.user.profils).isEmpty() && _(link.profils).includes( ctrl.user.profil_actif.profil_id );
-                                        } );
+                                    ctrl.$onInit = function() {
+                                        ctrl.help_links = _(CONFIG.help_links)
+                                            .select( function( link ) {
+                                                return !_(ctrl.user.profils).isEmpty() && _(link.profils).includes( ctrl.user.profil_actif.profil_id );
+                                            } );
+                                    };
                                 } ]
                 } );
