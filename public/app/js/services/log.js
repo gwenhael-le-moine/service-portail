7e3ec30bde2cde59946475e56188c58f3b6106aa
Daniel LACROIX <dlacroix@erasme.org>
2017-05-05 19:27:37 +0200

    change log API

--- public/app/js/services/log.js
+++ public/app/js/services/log.js
@@ -7,11 +7,11 @@ angular.module( 'portailApp' )
                     this.add = function( app, url, params ) {
                         currentUser.get( false )
                             .then( function( user ) {
-                                $http.post( URL_ENT + '/api/app/v2/log',
-                                            { app: app,
-                                              uid: UID,
-                                              uai: user.profil_actif ? user.profil_actif.etablissement_code_uai : 'none',
-                                              user_type: user.profil_actif ? user.profil_actif.profil_id : 'none',
+                                $http.post( URL_ENT + '/api/logs',
+                                            { application_id: app,
+                                              user_id: UID,
+                                              etablissement_id: user.profil_actif ? user.profil_actif.etablissement_code_uai : 'none',
+                                              profil_id: user.profil_actif ? user.profil_actif.profil_id : 'none',
                                               timestamp: Date.now(),
                                               url: _(url).isNull() ? APP_PATH + $state.current.url: url,
                                               params: _(params).isNull() ? _($state.params).map( function( value, key ) { return key + '=' + value; } ).join( '&' ) : params } );
