68eb0e48bc49c5be0e82adeb74099ef3f44625e9
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-05-30 16:53:11 +0200

    use resource

--- public/app/js/components/app_iframe.js
+++ public/app/js/components/app_iframe.js
@@ -4,18 +4,20 @@ angular.module( 'portailApp' )
     .component( 'appiframe',
                 { bindings: { appid: '<' },
                   templateUrl: 'app/js/components/app_iframe.html',
-                  controller: [ '$sce', 'Apps', 'Utils', 'currentUser',
-                                function ( $sce, Apps, Utils, currentUser ) {
+                  controller: [ '$sce', 'Apps', 'Utils', 'currentUser', 'User',
+                                function ( $sce, Apps, Utils, currentUser, User ) {
                                     var ctrl = this;
                                     ctrl.$onInit = function() {
                                         ctrl.iOS = ( navigator.userAgent.match( /iPad/i ) !== null ) || ( navigator.userAgent.match( /iPhone/i ) !== null );
                                         var apps_list = [];
 
                                         currentUser.get().then( function( response ) {
-                                            if ( _(response.data.profiles).isEmpty() ) {
+                                            var user =  new User( response.data );
+
+                                            if ( _(user.profiles).isEmpty() ) {
                                                 apps_list = Apps.query_defaults().$promise;
                                             } else {
-                                                apps_list = Apps.query({ uai: response.data.active_profile().structure_id }).$promise;
+                                                apps_list = Apps.query({ uai: user.active_profile().structure_id }).$promise;
                                             }
 
                                             apps_list.then( function ( response ) {
