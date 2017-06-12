4074c717c8421b33983299169eb5a5ca07ec18c9
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-06-12 16:39:16 +0200

    display user-friendly profiles

--- public/app/js/components/profil_actif.js
+++ public/app/js/components/profil_actif.js
@@ -4,9 +4,17 @@ angular.module( 'portailApp' )
     .component( 'profilactif',
                 { bindings: { user: '<' },
                   templateUrl: 'app/js/components/profil_actif.html',
-                  controller: [ '$state', '$stateParams', 'currentUser',
-                                function( $state, $stateParams, currentUser ) {
+                  controller: [ '$http', '$state', '$stateParams', 'currentUser', 'URL_ENT',
+                                function( $http, $state, $stateParams, currentUser, URL_ENT ) {
                                     var ctrl = this;
+
+                                    var get_structure = _.memoize( function( structure_id ) {
+                                        return $http.get( URL_ENT + '/api/structures/' + structure_id );
+                                    } );
+
+                                    var get_profile_type = _.memoize( function( type ) {
+                                        return $http.get( URL_ENT + '/api/profiles_types/' + type );
+                                    } );
 
                                     ctrl.apply_change = function() {
                                         currentUser.activate_profile( ctrl.current_profile.id )
@@ -17,6 +25,17 @@ angular.module( 'portailApp' )
                                     };
 
                                     ctrl.$onInit = function() {
+                                        ctrl.user.profiles.forEach( function( profile ) {
+                                            get_structure( profile.structure_id )
+                                                .then( function( response ) {
+                                                    profile.structure = response.data;
+                                                } );
+                                            get_profile_type( profile.type )
+                                                .then( function( response ) {
+                                                    profile.profile = response.data;
+                                                } );
+                                        } );
+
                                         ctrl.current_profile = ctrl.user.active_profile();
                                     };
                                 } ]
