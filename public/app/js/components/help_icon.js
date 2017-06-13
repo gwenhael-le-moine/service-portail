e952f1d96deca751013f7078dcb69286b5049efd
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-06-13 11:58:23 +0200

    fix help icon

--- public/app/js/components/help_icon.js
+++ public/app/js/components/help_icon.js
@@ -11,7 +11,8 @@ angular.module( 'portailApp' )
                                         currentUser.get().then( function( user ) {
                                             ctrl.help_links = _(CONFIG.help_links)
                                                 .select( function( link ) {
-                                                    return !_(user.profiles).isEmpty() && _(link.profils).includes( user.active_profile().type );
+                                                    return !_(user.profiles).isEmpty()
+                                                        && _(link.profils).intersection( _(user.profiles).pluck('type') ).length > 0;
                                                 } );
                                         } );
                                     };
