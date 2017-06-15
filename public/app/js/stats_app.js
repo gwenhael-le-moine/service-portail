33432ddaf92aa2fb491fbcdaab560f8c6e3245b3
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-06-15 16:16:51 +0200

    Stats app ported to new APIs

--- public/app/js/stats_app.js
+++ public/app/js/stats_app.js
@@ -11,18 +11,19 @@ angular.module( 'statsApp',
               [ '$http', 'URL_ENT',
                 function( $http, URL_ENT ) {
                     this.get_stats = function( params ) {
-                        return $http.get( URL_ENT + '/api/app/v2/log/stats', { params: params } );
+                        return $http.get( URL_ENT + '/api/logs', { params: params } );
                     };
 
                     this.get_profils = function( params ) {
-                        return $http.get( URL_ENT + '/api/app/profils', { params: params } );
+                        return $http.get( URL_ENT + '/api/profiles_types', { params: params } );
                     };
 
                     this.get_structures = function( params ) {
-                        return $http.get( URL_ENT + '/api/app/structures', { params: params } );
+                        params.expand = false;
+                        return $http.get( URL_ENT + '/api/structures', { params: params } );
                     };
 
-                    this.get_default_applications = function( params ) {
+                    this.get_applications = function( params ) {
                         return $http.get( URL_ENT + '/api/applications', { params: params } );
                     };
                 }
@@ -31,10 +32,10 @@ angular.module( 'statsApp',
                  [ '$scope', '$http', '$locale', 'moment', 'Annuaire',
                    function ( $scope, $http, $locale, moment, Annuaire ) {
                        $scope.types_labels = { global: 'Statistiques globales',
-                                               uai: 'Établissements',
-                                               app: 'Tuiles',
-                                               user_type: 'Profils utilisateurs',
-                                               uid: 'Utilisateurs',
+                                               structure_id: 'Établissements',
+                                               application_id: 'Tuiles',
+                                               profil_id: 'Profils utilisateurs',
+                                               user_id: 'Utilisateurs',
                                                week_day: 'Jour de la semaine'};
 
                        $scope.period_types = { list: [ { label: 'jour', value: 'day' },
@@ -71,8 +72,8 @@ angular.module( 'statsApp',
 
                        $scope.chart_options = function( type, data ) {
                            switch( type ) {
-                           case 'uai':
-                           case 'user_type':
+                           case 'structure_id':
+                           case 'profil_id':
                                $scope.multibarhorizontalchart_options.chart.height = 24 * data.length * data[0].values.length + 40;
                                $scope.multibarhorizontalchart_options.chart.margin.left = _.chain(data[0].values).pluck('x').map( function(label) { return label.length; } ).max().value() * 8;
                                return $scope.multibarhorizontalchart_options;
@@ -91,42 +92,42 @@ angular.module( 'statsApp',
                            $scope.labels.week_day.push( $scope.labels.week_day.shift() );
 
                            Annuaire.get_profils({}).then( function( response ) {
-                               $scope.labels.user_type = _.chain(response.data).map( function( profil ) { return [ profil.id, profil.description ]; } ).object().value();
+                               $scope.labels.profil_id = _.chain(response.data).map( function( profil ) { return [ profil.id, profil.name ]; } ).object().value();
                            } );
 
-                           Annuaire.get_default_applications({}).then( function( response ) {
-                               $scope.labels.app = _.chain(response.data).map( function( app ) { return [ app.id, app.libelle ]; } ).object().value();
+                           Annuaire.get_applications({}).then( function( response ) {
+                               $scope.labels.application_id = _.chain(response.data).map( function( app ) { return [ app.id, app.name ]; } ).object().value();
                            } );
 
 
                            Annuaire.get_structures({}).then( function( response ) {
-                               $scope.labels.uai = _.chain(response.data).map( function( etab ) { return [ etab.code_uai, etab.nom ]; } ).object().value();
+                               $scope.labels.structure_id = _.chain(response.data).map( function( etab ) { return [ etab.id, etab.name ]; } ).object().value();
 
-                               Annuaire.get_stats( { from: $scope.debut.clone().toDate(),
-                                                     until: $scope.fin.clone().toDate(),
-                                                     "uais[]": _.chain($scope.labels.uai)
+                               Annuaire.get_stats( { 'timestamp>': $scope.debut.clone().toDate(),
+                                                     'timestamp<': $scope.fin.clone().toDate(),
+                                                     "structure_id[]": _.chain($scope.labels.structure_id)
                                                      .keys()
-                                                     .reject( function( uai ) {
-                                                         var ignored_uai = _([ '0699990Z', '069BACAS', '069DANEZ' ]);
-                                                         return ignored_uai.contains( uai );
+                                                     .reject( function( structure_id ) {
+                                                         var ignored_structure_id = _([ '0699990Z', '069BACAS', '069DANEZ' ]);
+                                                         return ignored_structure_id.contains( structure_id );
                                                      } )
                                                      .value()
                                                    } )
                                    .then( function ( response ) {
-                                       var keys = [ 'uai', 'app', 'user_type', 'week_day' ];
+                                       var keys = [ 'structure_id', 'application_id', 'profil_id', 'week_day' ];
 
                                        var stats_to_nvd3_data = function( key, values ) {
                                            var data = [ { key: key,
                                                           values: _.chain(values).keys().map( function( subkey ) {
                                                               return { key: key,
                                                                        value: subkey,
-                                                                       x: $scope.labels[key][subkey],
+                                                                       x: $scope.labels[ key ][ subkey ],
                                                                        y: values[ subkey ] };
                                                           } )
                                                           .sortBy( function( record ) {
                                                               switch( key ) {
-                                                              case 'uai':
-                                                              case 'user_type':
+                                                              case 'structure_id':
+                                                              case 'profil_id':
                                                                   return record.y * -1;
                                                                   break;
                                                               case 'week_day':
@@ -170,43 +171,43 @@ angular.module( 'statsApp',
                                            }
                                        } );
 
-                                       $scope.stats.global.uai.push( { key: 'utilisateurs uniques',
-                                                                       values: _.chain($scope.logs)
-                                                                       .groupBy( function( line ) { return line.uai; } )
-                                                                       .map( function( loglines, uai ) {
-                                                                           return { key: 'utilisateurs uniques',
-                                                                                    x: $scope.labels.uai[uai],
-                                                                                    y: _.chain(loglines).pluck('uid').uniq().value().length };
-                                                                       } ).value()
-                                                                     } );
-                                       $scope.stats.global.uai.push( { key: 'apps',
-                                                                       values: _.chain($scope.logs)
-                                                                       .groupBy( function( line ) { return line.uai; } )
-                                                                       .map( function( loglines, uai ) {
-                                                                           return { key: 'apps',
-                                                                                    x: $scope.labels.uai[uai],
-                                                                                    y: _.chain(loglines).pluck('app').uniq().value().length };
-                                                                       } ).value()
-                                                                     } );
-                                       $scope.stats.global.user_type.push( { key: 'utilisateurs uniques',
+                                       $scope.stats.global.structure_id.push( { key: 'utilisateurs uniques',
+                                                                                values: _.chain($scope.logs)
+                                                                                .groupBy( function( line ) { return line.structure_id; } )
+                                                                                .map( function( loglines, structure_id ) {
+                                                                                    return { key: 'utilisateurs uniques',
+                                                                                             x: $scope.labels.structure_id[ structure_id ],
+                                                                                             y: _.chain(loglines).pluck('user_id').uniq().value().length };
+                                                                                } ).value()
+                                                                              } );
+                                       $scope.stats.global.structure_id.push( { key: 'apps',
+                                                                                values: _.chain($scope.logs)
+                                                                                .groupBy( function( line ) { return line.structure_id; } )
+                                                                                .map( function( loglines, structure_id ) {
+                                                                                    return { key: 'apps',
+                                                                                             x: $scope.labels.structure_id[structure_id],
+                                                                                             y: _.chain(loglines).pluck('application_id').uniq().value().length };
+                                                                                } ).value()
+                                                                              } );
+                                       $scope.stats.global.profil_id.push( { key: 'utilisateurs uniques',
                                                                              values: _.chain($scope.logs)
-                                                                             .groupBy( function( line ) { return line.user_type; } )
-                                                                             .map( function( loglines, user_type ) {
+                                                                             .groupBy( function( line ) { return line.profil_id; } )
+                                                                             .map( function( loglines, profil_id ) {
                                                                                  return { key: 'utilisateurs uniques',
-                                                                                          x: $scope.labels.user_type[user_type],
-                                                                                          y: _.chain(loglines).pluck('uid').uniq().value().length };
+                                                                                          x: $scope.labels.profil_id[profil_id],
+                                                                                          y: _.chain(loglines).pluck('user_id').uniq().value().length };
                                                                              } ).value()
                                                                            } );
 
-                                       _($scope.stats.uai).each( function( etab, uai ) {
-                                           etab.user_type.push( { key: 'utilisateurs uniques',
+                                       _($scope.stats.structure_id).each( function( etab, structure_id ) {
+                                           etab.profil_id.push( { key: 'utilisateurs uniques',
                                                                   values: _.chain($scope.logs)
-                                                                  .where({ uai: uai })
-                                                                  .groupBy( function( line ) { return line.user_type; } )
-                                                                  .map( function( loglines, user_type ) {
+                                                                  .where({ structure_id: structure_id })
+                                                                  .groupBy( function( line ) { return line.profil_id; } )
+                                                                  .map( function( loglines, profil_id ) {
                                                                       return { key: 'utilisateurs uniques',
-                                                                               x: $scope.labels.user_type[user_type],
-                                                                               y: _.chain(loglines).pluck('uid').uniq().value().length };
+                                                                               x: $scope.labels.profil_id[ profil_id ],
+                                                                               y: _.chain(loglines).pluck('user_id').uniq().value().length };
                                                                   } ).value()
                                                                 } );
                                        } );
