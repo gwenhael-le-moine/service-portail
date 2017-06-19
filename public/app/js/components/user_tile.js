d064c569412d14f68a1b877df5699f3039b2381b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-06-19 10:59:04 +0200

    wait for user info to arrive

--- public/app/js/components/user_tile.js
+++ public/app/js/components/user_tile.js
@@ -2,19 +2,22 @@
 
 angular.module( 'portailApp' )
     .component( 'userTile',
-                { bindings: { user: '=' },
+                { bindings: { user: '<' },
                   templateUrl: 'app/js/components/user_tile.html',
                   controller: [ 'APP_PATH', 'URL_ENT',
                                 function( APP_PATH, URL_ENT ) {
                                     var ctrl = this;
 
                                     ctrl.APP_PATH = APP_PATH;
-                                    ctrl.URL_ENT = URL_ENT;
 
                                     ctrl.change_password_message = 'Pensez à changer votre mot de passe...';
 
                                     ctrl.edit_profile = function() {
                                         ctrl.user.edit_profile = !ctrl.user.edit_profile;
+                                    };
+
+                                    ctrl.$onInit = function() {
+                                        ctrl.url_avatar = URL_ENT + '/' + ctrl.user.avatar;
                                     };
                                 } ]
                 } );
