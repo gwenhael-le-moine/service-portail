a1df40f0b39da691ff4a5c7b96ea0487526bfccf
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-09-06 15:14:27 +0200

    fix user-menu

--- public/app/js/components/user_menu.js
+++ public/app/js/components/user_menu.js
@@ -12,23 +12,17 @@ angular.module( 'portailApp' )
                                     };
                                 } ],
                   template: `
-<div class="dropdown hidden-xs hidden-sm hidden-md pull-right user-menu" uib-dropdown>
-    <a class="dropdown-toggle" id="dropdownMenu1" uib-dropdown-toggle>
-        {{$ctrl.user.prenom}} {{$ctrl.user.nom}}
+<div class="uib-dropdown hidden-xs hidden-sm hidden-md pull-right user-menu" uib-dropdown>
+    <a class="uib-dropdown-toggle" id="dropdownMenu1" uib-dropdown-toggle>
+        {{$ctrl.user.firstname}} {{$ctrl.user.lastname}}
     </a>
-    <ul class="gris3 dropdown-menu" role="menu" aria-labelledby="dropdownMenu1" uib-dropdown-menu>
+    <ul class="gris3 uib-dropdown-menu" role="menu" aria-labelledby="dropdownMenu1" uib-dropdown-menu>
         <li>
             <a ng:click="$ctrl.go_home()"
                ng:style="{'background-image': 'url(' + $ctrl.prefix +'/node_modules/laclasse-common-client/images/logolaclasse.svg)' }" >
                 retour au portail
             </a>
         </li>
-        <!-- <li>
-             <a data-ui-sref="app({ appid: 'aide' })"
-             ng:style="{'background-image': 'url(' + $ctrl.prefix + '/node_modules/laclasse-common-client/images/12_aide.svg)' }" >
-             aide
-             </a>
-             </li> -->
         <li>
             <a ng:href="{{$ctrl.prefix}}/logout"
                ng:style="{'background-image': 'url(' + $ctrl.prefix + '/node_modules/laclasse-common-client/images/12_aide.svg)' }" >
