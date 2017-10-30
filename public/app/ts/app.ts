be5f8251ae56807c50042536e70a75ca483c6c47
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-10-30 11:51:52 +0100

    npm update
    Signed-off-by: Gwenhael Le Moine <gwenhael.le.moine@gmail.com>

--- public/app/ts/app.ts
+++ public/app/ts/app.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module( 'portailApp', [ 'ngResource',
+angular.module('portailApp', ['ngResource',
   'ui.router',
   'ui.bootstrap',
   'as.sortable',
@@ -12,55 +12,53 @@ angular.module( 'portailApp', [ 'ngResource',
   'angular-carousel',
   'ngFitText',
   'zxcvbn',
-  'toastr' ] )
-  .config( [ '$stateProvider', '$urlRouterProvider', '$locationProvider', 'APP_PATH',
-    function( $stateProvider, $urlRouterProvider, $locationProvider, APP_PATH ) {
-      // $locationProvider.html5Mode(true);
-
+  'toastr'])
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'APP_PATH',
+    function($stateProvider, $urlRouterProvider, $locationProvider, APP_PATH) {
       $stateProvider
-        .state( 'portail',
+        .state('portail',
         {
           url: '/',
           component: 'portail'
-        } )
-        .state( 'app',
+        })
+        .state('app',
         {
           url: '/app/:appid',
           component: 'appWrapper',
           resolve: {
-            appId: [ '$transition$',
-              function( $transition$ ) {
+            appId: ['$transition$',
+              function($transition$) {
                 return $transition$.params().appid;
               }]
           }
-        } );
+        });
 
-      $urlRouterProvider.otherwise( '/' );
+      $urlRouterProvider.otherwise('/');
     }
-  ] )
-  .config( [ '$httpProvider',
-    function( $httpProvider ) {
+  ])
+  .config(['$httpProvider',
+    function($httpProvider) {
       $httpProvider.defaults.useXDomain = true;
       $httpProvider.defaults.withCredentials = true;
 
-      $httpProvider.interceptors.push( [ '$q', '$window',
-        function( $q, $window ) {
+      $httpProvider.interceptors.push(['$q', '$window',
+        function($q, $window) {
           return {
-            'responseError': function( rejection ) {
-              if ( rejection.status === 401 ) {
-                $window.location.href = '/sso/login?ticket=false&service=' + encodeURIComponent( $window.location.href );
+            'responseError': function(rejection) {
+              if (rejection.status === 401) {
+                $window.location.href = '/sso/login?ticket=false&service=' + encodeURIComponent($window.location.href);
               }
               return rejection;
             }
           };
-        }] );
-    }] )
-  .run( [ '$rootScope', 'log',
-    function( $rootScope, log ) {
+        }]);
+    }])
+  .run(['$rootScope', 'log',
+    function($rootScope, log) {
       $rootScope.modification = false;
-      $rootScope.$on( '$stateChangeSuccess',
-        function( event, toState, toParams, fromState, fromParams ) {
-          log.add( ( toState.name == 'app' ) ? toParams.appid : 'PORTAIL', null, null );
-        } );
+      $rootScope.$on('$stateChangeSuccess',
+        function(event, toState, toParams, fromState, fromParams) {
+          log.add((toState.name == 'app') ? toParams.appid : 'PORTAIL', null, null);
+        });
     }
-  ] );
+  ]);
