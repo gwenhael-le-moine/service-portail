0a75b31edf29581037897d66ef7e5acfd5fc42d0
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-11-22 11:50:53 +0100

    disable new logo

--- public/app/ts/components/logo.ts
+++ public/app/ts/components/logo.ts
@@ -19,7 +19,7 @@ angular.module('portailApp')
     template: `
 <a ng:click="$ctrl.go_home()">
   <img draggable="false" ng:src="{{$ctrl.prefix}}/app/node_modules/laclasse-common-client/images/logolaclasse.svg" />
-<h3 class="hidden-xs hidden-sm ent-name" style="background-image: url('/portail/app/node_modules/laclasse-common-client/images/grandlyon-logo-blanc.svg');background-size: contain;background-repeat: no-repeat;background-position-x: right;background-position-y: center; background-color: #da0000; background-blend-mode: overlay; width: 300px;height: 42px; text-align: left;margin-right: 0px;">laclasse.com</h3>
+<h3 class="hidden-xs hidden-sm ent-name" style="/*background-image: url('/portail/app/node_modules/laclasse-common-client/images/grandlyon-logo-blanc.svg');background-size: contain;background-repeat: no-repeat;background-position-x: right;background-position-y: center; background-color: #da0000; background-blend-mode: overlay; width: 300px;height: 42px; text-align: left;margin-right: 0px;*/">laclasse.com</h3>
 </a>
 `
   });
