09f5ba8ef8fe3c0cf6c9d45f386dce388aeb15c9
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-03-05 14:11:11 +0100

    spinner out of label

--- public/app/ts/stats_app.ts
+++ public/app/ts/stats_app.ts
@@ -13,73 +13,73 @@ angular.module('statsApp',
     {
       template: `
 <style>
-  .loader,
-  .loader:before,
-  .loader:after {
-  border-radius: 50%;
-  }
-  .loader {
-  color: #1aaacc;
-  font-size: 11px;
-  text-indent: -99999em;
-  margin: 55px auto;
-  position: relative;
-  width: 10em;
-  height: 10em;
-  box-shadow: inset 0 0 0 1em;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  }
-  .loader:before,
-  .loader:after {
-  position: absolute;
-  content: '';
-  }
-  .loader:before {
-  width: 5.2em;
-  height: 10.2em;
-  background: #ffffff;
-  border-radius: 10.2em 0 0 10.2em;
-  top: -0.1em;
-  left: -0.1em;
-  -webkit-transform-origin: 5.2em 5.1em;
-  transform-origin: 5.2em 5.1em;
-  -webkit-animation: load2 2s infinite ease 1.5s;
-  animation: load2 2s infinite ease 1.5s;
-  }
-  .loader:after {
-  width: 5.2em;
-  height: 10.2em;
-  background: #ffffff;
-  border-radius: 0 10.2em 10.2em 0;
-  top: -0.1em;
-  left: 5.1em;
-  -webkit-transform-origin: 0px 5.1em;
-  transform-origin: 0px 5.1em;
-  -webkit-animation: load2 2s infinite ease;
-  animation: load2 2s infinite ease;
-  }
-  @-webkit-keyframes load2 {
-  0% {
-  -webkit-transform: rotate(0deg);
-  transform: rotate(0deg);
-  }
-  100% {
-  -webkit-transform: rotate(360deg);
-  transform: rotate(360deg);
-  }
-  }
-  @keyframes load2 {
-  0% {
-  -webkit-transform: rotate(0deg);
-  transform: rotate(0deg);
-  }
-  100% {
-  -webkit-transform: rotate(360deg);
-  transform: rotate(360deg);
-  }
-  }
+.loader,
+.loader:before,
+.loader:after {
+border-radius: 50%;
+}
+.loader {
+color: #1aaacc;
+font-size: 11px;
+text-indent: -99999em;
+margin: 55px auto;
+position: relative;
+width: 10em;
+height: 10em;
+box-shadow: inset 0 0 0 1em;
+-webkit-transform: translateZ(0);
+-ms-transform: translateZ(0);
+transform: translateZ(0);
+}
+.loader:before,
+.loader:after {
+position: absolute;
+content: '';
+}
+.loader:before {
+width: 5.2em;
+height: 10.2em;
+background: #ffffff;
+border-radius: 10.2em 0 0 10.2em;
+top: -0.1em;
+left: -0.1em;
+-webkit-transform-origin: 5.2em 5.1em;
+transform-origin: 5.2em 5.1em;
+-webkit-animation: load2 2s infinite ease 1.5s;
+animation: load2 2s infinite ease 1.5s;
+}
+.loader:after {
+width: 5.2em;
+height: 10.2em;
+background: #ffffff;
+border-radius: 0 10.2em 10.2em 0;
+top: -0.1em;
+left: 5.1em;
+-webkit-transform-origin: 0px 5.1em;
+transform-origin: 0px 5.1em;
+-webkit-animation: load2 2s infinite ease;
+animation: load2 2s infinite ease;
+}
+@-webkit-keyframes load2 {
+0% {
+-webkit-transform: rotate(0deg);
+transform: rotate(0deg);
+}
+100% {
+-webkit-transform: rotate(360deg);
+transform: rotate(360deg);
+}
+}
+@keyframes load2 {
+0% {
+-webkit-transform: rotate(0deg);
+transform: rotate(0deg);
+}
+100% {
+-webkit-transform: rotate(360deg);
+transform: rotate(360deg);
+}
+}
 </style>
 <div class="loader">Loading...</div>
 `})
@@ -441,7 +441,8 @@ angular.module('statsApp',
         <span class="label label-primary">Aucune donnée disponible pour la période donnée.</span>
       </h1>
       <h1 style="text-align: center;" ng:if="$ctrl.raw_logs.length == 0 && $ctrl.loading">
-        <span class="label label-warning">Chargement et traitement des données en cours <loader-spinner></loader-spinner></span>
+        <span class="label label-warning">Chargement et traitement des données en cours...</span>
+        <loader-spinner></loader-spinner>
       </h1>
       <div ng:if="$ctrl.raw_logs.length > 0 && !$ctrl.loading">
         <div class="col-md-12">
